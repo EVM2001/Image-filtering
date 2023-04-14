e72213e280dac90d99475cbd8208e270d55283dc
EVM2001 <130757152+EVM2001@users.noreply.github.com>
2023-04-14 15:25:01 +0400

    Add files via upload

---
+++ Diplom/Diplom/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿
+namespace Diplom
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtImageSize = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtImageSize);
+            this.groupBox2.Location = new System.Drawing.Point(279, 10);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(155, 55);
+            this.groupBox2.TabIndex = 14;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Размер изображения";
+            // 
+            // txtImageSize
+            // 
+            this.txtImageSize.Enabled = false;
+            this.txtImageSize.Location = new System.Drawing.Point(15, 20);
+            this.txtImageSize.Name = "txtImageSize";
+            this.txtImageSize.Size = new System.Drawing.Size(130, 20);
+            this.txtImageSize.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtFileName);
+            this.groupBox1.Location = new System.Drawing.Point(118, 10);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(155, 55);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Имя загруженного файла";
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Enabled = false;
+            this.txtFileName.Location = new System.Drawing.Point(15, 20);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.Size = new System.Drawing.Size(130, 20);
+            this.txtFileName.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(12, 72);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(521, 468);
+            this.panel1.TabIndex = 12;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(500, 460);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 25);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Загрузить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.GetAndShowFile);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(483, 28);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Test Filter";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Filter1);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(564, 28);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "Median Filter";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Median);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.pictureBox2.Location = new System.Drawing.Point(614, 72);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(500, 460);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 17;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(808, 27);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 18;
+            this.button4.Text = "сохранить(тест)";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Visible = false;
+            this.button4.Click += new System.EventHandler(this.SaveImage);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(938, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 19;
+            this.textBox1.Visible = false;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(645, 28);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 20;
+            this.button5.Text = "Gauss Filter";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.GaussFilter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1188, 683);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtImageSize;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtFileName;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Diplom/Diplom/Form1.cs
@@ -0,0 +1,340 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+namespace Diplom
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+            GaussFilterCreation(GKernel);
+        }
+        double[,] GKernel = new double[5, 5];
+        string FilePath;
+        string Filename;
+        int width, height;//ширина и высота        
+        Bitmap bitmap;//элемент класса для работы с пикселями
+        Bitmap grayNoise;
+
+        //double[,] mask = { { 0.1, 0.1, 0.1 }, { 0.1, 0.2, 0.1 }, { 0.1, 0.1, 0.1 } };//сглаживающий фильтр
+
+        //int[,] mask = { { 0, -1, 0 }, { -1, 5, -1 }, { 0, -1, 0 } };//контрастоповышающий фильтр
+        //int[,] mask = { { -1, -1, -1 }, { -1, 9, -1 }, { -1, -1, -1 } };//контрастоповышающий фильтр
+
+        int[,] mask = { { 0, 1, 0 }, { 1, -4, 1 }, { 0, 1, 0 } };//фильтр Лапласа
+        //int[,] mask = { { -1, -1, -1 }, { -1, 8, -1 }, { -1, -1, -1 } };
+
+        //int[,] mask = { { -1, 0, 1 }, { -1, 0, 1 }, { -1, 0, 1 } };//Фильтр Превитта
+
+
+        //реализация кнопки "загрузить"
+        private void GetAndShowFile(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    //pictureBox1.Image = null;//убрать старое изображение ???
+
+                    FilePath = openFileDialog1.FileName;//задание пути к файлу
+                    Filename = openFileDialog1.SafeFileName;//задание имени файла
+                    Image NewImage = Image.FromFile(FilePath);//создание объекта типа "изображение"
+                    width = NewImage.Width;//получение ширины
+                    height = NewImage.Height;//и высоты
+
+
+                    bitmap = new Bitmap(NewImage);//заполнение элемента класса для работы с пикселями(оригинальное изображение)
+
+                    //Bitmap gray = CreateGray(bitmap);
+                    //grayNoise = CreateNoise(CreateGray(bitmap));//перевод оригинального изображения в серый цвет и добавление шума
+                    grayNoise = CreateNoise(bitmap);
+
+                    pictureBox1.Image = grayNoise;//визуализация изображения
+                    pictureBox2.Image = bitmap;
+
+                    //pictureBox1.Image = bitmap;
+
+                    txtFileName.Text = Filename;//вывод имени файла
+                    txtImageSize.Text = width.ToString() + " X " + height.ToString();//вывод размера файла
+
+                    //textBox1.Text = mask[0,1].ToString();
+
+                    //GaussFormula(-2, -2);
+                    
+                    //button2.Enabled = true;//разрешение на использование фильтра
+                }
+                catch
+                {
+                    MessageBox.Show("Выберите файл-изображение");
+                    //button2.Enabled = false;
+                }
+            }
+        }
+        private void Filter1(object sender, EventArgs e)
+        {
+            Bitmap FilteredImage = new Bitmap(width, height);
+
+            /*for (int w = 0; w < width; w++)
+            {
+                FilteredImage.SetPixel(w, 0, bitmap.GetPixel(w, 0));
+                FilteredImage.SetPixel(w, height - 1, bitmap.GetPixel(w, height - 1));
+            }            
+            for (int h = 0; h < height; h++)
+            {
+                FilteredImage.SetPixel(0, h, bitmap.GetPixel(0, h));
+                FilteredImage.SetPixel(width - 1, h, bitmap.GetPixel(width - 1, h));
+            }*/
+
+
+
+            for (int h = 1; h < height - 1; h++)
+            {
+                for (int w = 1; w < width - 1; w++)
+                {
+                    int color = (int)ForFilter1(w, h);
+                    //FilteredImage.SetPixel(w, h, Color.FromArgb(ForFilter1(w, h)));
+                    FilteredImage.SetPixel(w, h, Color.FromArgb(color, color, color));
+                }
+            }
+            grayNoise = FilteredImage;
+            //bitmap = FilteredImage;
+            pictureBox1.Image = FilteredImage;
+        }
+        private double ForFilter1(int w, int h)
+        {
+            double newcolor = 0;
+            
+
+            for (int i = h - 1; i < h + 2; i++)
+            {
+                for (int j = w - 1; j < w + 2; j++)
+                {
+                    //newcolor += mask[i - (h - 1), j - (w - 1)] * grayNoise.GetPixel(j, i).ToArgb();
+                    int test = grayNoise.GetPixel(j, i).R;
+                    double test2 = mask[i - (h - 1), j - (w - 1)];
+                    newcolor += mask[i - (h - 1), j - (w - 1)] * grayNoise.GetPixel(j, i).R;
+                }
+            }
+
+            /*newcolor = mask[0, 0] * bitmap.GetPixel(w - 1, h - 1).ToArgb() + mask[0, 1] * bitmap.GetPixel(w, h - 1).ToArgb() + mask[0, 2] * bitmap.GetPixel(w + 1, h - 1).ToArgb()
+                + mask[1, 0] * bitmap.GetPixel(w - 1, h).ToArgb() + mask[1, 1] * bitmap.GetPixel(w, h).ToArgb() + mask[1, 2] * bitmap.GetPixel(w + 1, h).ToArgb()
+                + mask[2, 0] * bitmap.GetPixel(w - 1, h + 1).ToArgb() + mask[2, 1] * bitmap.GetPixel(w, h + 1).ToArgb() + mask[2, 2] * bitmap.GetPixel(w + 1, h + 1).ToArgb();*/
+
+
+            return newcolor;
+        }
+
+        private void Median(object sender, EventArgs e)
+        {
+            grayNoise = MedianFilter(grayNoise, 3);
+            pictureBox1.Image = grayNoise;
+        }
+
+        private Bitmap MedianFilter(Bitmap sourceBitmap, int matrixSize, bool grayscale = false)
+        {
+            BitmapData sourceData = sourceBitmap.LockBits(new Rectangle(0, 0, sourceBitmap.Width, sourceBitmap.Height), ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+            byte[] pixelBuffer = new byte[sourceData.Stride * sourceData.Height];
+            byte[] resultBuffer = new byte[sourceData.Stride * sourceData.Height];
+            Marshal.Copy(sourceData.Scan0, pixelBuffer, 0, pixelBuffer.Length);
+            sourceBitmap.UnlockBits(sourceData);
+
+            /*if (grayscale == true)
+            {
+                float rgb = 0;
+                for (int k = 0; k < pixelBuffer.Length; k += 4)
+                {
+                    rgb = pixelBuffer[k] * 0.11f;
+                    rgb += pixelBuffer[k + 1] * 0.59f;
+                    rgb += pixelBuffer[k + 2] * 0.3f;
+
+
+                    pixelBuffer[k] = (byte)rgb;
+                    pixelBuffer[k + 1] = pixelBuffer[k];
+                    pixelBuffer[k + 2] = pixelBuffer[k];
+                    pixelBuffer[k + 3] = 255;
+                }
+            }*/
+            int filterOffset = (matrixSize - 1) / 2;
+            int calcOffset = 0;
+            int byteOffset = 0;
+            List<int> neighbourPixels = new List<int>();
+            byte[] middlePixel;
+
+            for (int offsetY = filterOffset; offsetY < sourceBitmap.Height - filterOffset; offsetY++)
+            {
+                for (int offsetX = filterOffset; offsetX < sourceBitmap.Width - filterOffset; offsetX++)
+                {
+                    byteOffset = offsetY * sourceData.Stride + offsetX * 4;
+                    neighbourPixels.Clear();
+                    for (int filterY = -filterOffset; filterY <= filterOffset; filterY++)
+                    {
+                        for (int filterX = -filterOffset; filterX <= filterOffset; filterX++)
+                        {
+                            calcOffset = byteOffset + (filterX * 4) + (filterY * sourceData.Stride);
+                            neighbourPixels.Add(BitConverter.ToInt32(pixelBuffer, calcOffset));
+                        }
+                    }
+                    neighbourPixels.Sort();
+                    middlePixel = BitConverter.GetBytes(neighbourPixels[filterOffset]);
+                    resultBuffer[byteOffset] = middlePixel[0];
+                    resultBuffer[byteOffset + 1] = middlePixel[1];
+                    resultBuffer[byteOffset + 2] = middlePixel[2];
+                    resultBuffer[byteOffset + 3] = middlePixel[3];
+                }
+            }
+
+            Bitmap resultBitmap = new Bitmap(sourceBitmap.Width, sourceBitmap.Height);
+            BitmapData resultData = resultBitmap.LockBits(new Rectangle(0, 0, resultBitmap.Width, resultBitmap.Height), ImageLockMode.WriteOnly, PixelFormat.Format32bppArgb);
+            Marshal.Copy(resultBuffer, 0, resultData.Scan0, resultBuffer.Length);
+            resultBitmap.UnlockBits(resultData);
+            return resultBitmap;
+        }
+
+        private Bitmap CreateGray(Bitmap bitmap)
+        {
+            // создаём Bitmap для черно-белого изображения
+            Bitmap output = new Bitmap(bitmap.Width, bitmap.Height);
+            // перебираем в циклах все пиксели исходного изображения
+            for (int j = 0; j < bitmap.Height; j++)
+                for (int i = 0; i < bitmap.Width; i++)
+                {
+                    // получаем (i, j) пиксель
+                    UInt32 pixel = (UInt32)(bitmap.GetPixel(i, j).ToArgb());
+                    // получаем компоненты цветов пикселя
+                    float R = (float)((pixel & 0x00FF0000) >> 16); // красный
+                    float G = (float)((pixel & 0x0000FF00) >> 8); // зеленый
+                    float B = (float)(pixel & 0x000000FF); // синий
+                                                           // делаем цвет черно-белым (оттенки серого) - находим среднее арифметическое
+                    R = G = B = (R + G + B) / 3.0f;
+                    // собираем новый пиксель по частям (по каналам)
+                    UInt32 newPixel = 0xFF000000 | ((UInt32)R << 16) | ((UInt32)G << 8) | ((UInt32)B);
+                    // добавляем его в Bitmap нового изображения
+                    output.SetPixel(i, j, Color.FromArgb((int)newPixel));
+                }
+            // выводим черно-белый Bitmap в pictureBox2
+            return output;
+
+        }      
+
+        private Bitmap CreateNoise(Bitmap bitmap)
+        {
+            Random rnd = new Random();
+            Bitmap output = bitmap.Clone(new Rectangle(0, 0, bitmap.Width, bitmap.Height), bitmap.PixelFormat);
+
+            for (int x = 0; x < output.Width; x++)
+            {
+                for (int y = 0; y < output.Height; y++)
+                {
+                    int q = rnd.Next(100);
+                    if (q <= 40) output.SetPixel(x, y, Color.White);
+                }
+            }
+            return output;
+        }
+
+        private void SaveImage(object sender, EventArgs e)
+        {
+            pictureBox1.Image.Save("E:\\мой ИНСТИТУТ\\4 курс\\диплом\\изображения\\TestNoiseImage2.jpg");
+        }
+
+        private void GaussFilter(object sender, EventArgs e)
+        {
+            Bitmap GaussFilteredImage = new Bitmap(width, height);
+
+            for (int h = 2; h < height - 2; h++)
+            {
+                for (int w = 2; w < width - 2; w++)
+                {
+                    int test = (int)ForGaussFilter(w, h);
+                    //Color test2 = Color.FromArgb((int)test, (int)test, (int)test);
+                    GaussFilteredImage.SetPixel(w, h, Color.FromArgb(test, test, test));                   
+                }
+            }
+            grayNoise = GaussFilteredImage;
+            //bitmap = GaussFilteredImage;
+            pictureBox1.Image = GaussFilteredImage;
+        }
+
+        private double ForGaussFilter(int w, int h)
+        {
+            double newcolor = 0;
+
+
+            /*for (int i = h - 3; i < h + 4; i++)
+            {
+                for (int j = w - 3; j < w + 4; j++)
+                {
+                    newcolor += (int)(GaussFormula(j - (w - 3) - 3, i - (h - 3) - 3) * grayNoise.GetPixel(j, i).ToArgb());
+                }
+            }*/
+
+            for (int i = h - 2; i < h + 3; i++)
+            {
+                for (int j = w - 2; j < w + 3; j++)
+                {
+                    //newcolor += GaussFormula(j - (w - 2) - 2, i - (h - 2) - 2) * grayNoise.GetPixel(j, i).R;
+
+                    //Color test = grayNoise.GetPixel(j, i);
+                    //int test2 = grayNoise.GetPixel(j, i).ToArgb();
+                    //double pixcolor = 0.21 * grayNoise.GetPixel(j, i).R + 0.72 * grayNoise.GetPixel(j, i).G + 0.07 * grayNoise.GetPixel(j, i).B;
+
+
+                    //newcolor += GKernel[i - (h - 2), j - (w - 2)] * pixcolor;
+
+
+                    newcolor += GKernel[i - (h - 2), j - (w - 2)] * grayNoise.GetPixel(j, i).R;
+                }
+            }
+
+            return newcolor;
+        }
+
+        private double GaussFormula(int x,int y)
+        {
+            double sig = 0.84089642;
+            //double sig = 1;
+            double formula = 1 / (2 * Math.PI * sig * sig) * Math.Exp(-(x * x + y * y) / (2 * sig * sig));
+            //textBox1.Text = formula.ToString();
+            return formula;
+        }
+
+        void GaussFilterCreation(double [,]GKernel)
+        {
+            // initialising standard deviation to 1.0
+            double sigma = 1.0;
+            double r, s = 2.0 * sigma * sigma;
+
+            // sum is for normalization
+            double sum = 0.0;
+
+            // generating 5x5 kernel
+            for (int x = -2; x <= 2; x++)
+            {
+                for (int y = -2; y <= 2; y++)
+                {
+                    r = Math.Sqrt(x * x + y * y);
+                    GKernel[x + 2,y + 2] = (Math.Exp(-(r * r) / s)) / (Math.PI * s);
+                    sum += GKernel[x + 2,y + 2];
+                }
+            }
+
+            // normalising the Kernel
+            for (int i = 0; i < 5; ++i)
+                for (int j = 0; j < 5; ++j)
+                    GKernel[i,j] /= sum;
+        }
+
+
+    }
+}
